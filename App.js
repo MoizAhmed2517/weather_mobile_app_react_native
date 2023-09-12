f8fbc226fe4c3733eeb5ef3a3b0428180b8a30aa
HP <ahmedmoiz962@gmail.com>
2023-09-12 22:51:57 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+
+  return (
+    <>
+      <StatusBar barStyle='dark-content' backgroundColor='#8ECDDD' />
+      <Text>Hello world React native app</Text>
+    </>
+  );
+}
+
+
+
+export default App;
