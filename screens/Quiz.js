7bd3f28f386063eace818091a92a466ede3490dd
HP <ahmedmoiz962@gmail.com>
2023-09-14 18:10:37 +0500

    v1.2

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -2,61 +2,121 @@ import { StyleSheet, Text, View, TouchableOpacity  } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import Result from './Result'
 
+const shuffleArray=(array)=> {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 export default function Quiz({navigation}) {
   const [questions, setQuestions] = useState([]);
+  const [ques, setQues] = useState(0);
+  const [options, setOptions] = useState([]);
+  const [score, setScore]= useState(0);
+  const [isloading, setIsloading] = useState(false)
   
   const getQuiz = async() => {
-
-  }
+    setIsloading(true)
+    const url ="https://opentdb.com/api.php?amount=10&type=multiple";
+    const res = await fetch(url);
+    const data = await res.json();
+    setQuestions(data.results);
+    setOptions(generateOptionsAndShuffle(data.results[0]))
+    setIsloading(false)
+  };
 
   useEffect(() => {
     getQuiz();
-  }, [third])
+  }, [])
+
+  const handleNextPress=()=>{
+    setQues(ques+1)
+    setOptions(generateOptionsAndShuffle(questions[ques+1]))
+  }
+
+  const generateOptionsAndShuffle=(_question)=>{
+    const options= [..._question.incorrect_answers]
+    options.push(_question.correct_answer)
+    shuffleArray(options)
+    return options
+  }
+
+  const handlSelectedOption=(_option)=>{
+    if(_option===questions[ques].correct_answer){
+      setScore(score+10)
+    }
+    if(ques!==9){
+      setQues(ques+1)
+      setOptions(generateOptionsAndShuffle(questions[ques+1]))
+    }
+    if(ques===9){
+      handleShowResult()
+    }
+  }
+
+  const handleShowResult=()=>{
+    navigation.navigate('Result', {
+      score: score
+    })
+  }
   
 
   return (
     <View style={styles.container}>
+
+
+      {isloading ?
+
+      <View>
+        <Text style={styles.loadingText}>Loading...</Text>
+      </View> :
+
+      questions.length > 0 && (
+
+        <View style={styles.parent}>
+
+          <View style={styles.question}>
+            <Text style={styles.questionText}>Q. {questions[ques].question}</Text>
+          </View>
+
+          <View style={styles.options}> 
+
+            <TouchableOpacity style={styles.optionButton} onPress={()=>handlSelectedOption(options[0])}>
+              <Text style={styles.optionText}>{options[0]}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.optionButton} onPress={()=>handlSelectedOption(options[1])}>
+              <Text style={styles.optionText}>{options[1]}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.optionButton} onPress={()=>handlSelectedOption(options[2])}>
+              <Text style={styles.optionText}>{options[2]}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.optionButton} onPress={()=>handlSelectedOption(options[3])}>
+              <Text style={styles.optionText}>{options[3]}</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <View style={styles.buttons}>
+
+            {ques!==9 &&<TouchableOpacity style={styles.button} onPress={handleNextPress}>
+              <Text style={styles.buttonText}>SKIP</Text>
+            </TouchableOpacity> }
+
+            {ques===9 &&<TouchableOpacity style={styles.button} onPress={handleShowResult}>
+              <Text style={styles.buttonText}>SHOW RESULTS</Text>
+            </TouchableOpacity> }
+
+          </View>
+
+        </View>
+
+        )
+      }
       
-      <View style={styles.question}>
-        <Text style={styles.questionText}>Q. Imagine this is a really cool question</Text>
-      </View>
-
-      <View style={styles.options}> 
-
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.optionText}>Cool option 1</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.optionText}>Cool option 2</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.optionText}>Cool option 3</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.optionText}>Cool option 4</Text>
-        </TouchableOpacity>
-
-      </View>
-
-      <View style={styles.buttons}>
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>SKIP</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>NEXT</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigation.navigate(Result)} style={styles.button}>
-        <Text style={styles.buttonText}>END</Text>
-        </TouchableOpacity>
-
-      </View>
-
     </View>
   )
 }
@@ -66,6 +126,9 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingHorizontal: 20,
     height: '100%'
+  },
+  parent: {
+    height: '100%',
   },
   question: {
     marginVertical: 16,
@@ -81,7 +144,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   button: {
-    width: 'auto',
+    width: '100%',
     backgroundColor: '#edae49',
     height: 'auto',
     justifyContent: 'center',
@@ -89,7 +152,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     padding: 10,
     paddingHorizontal: 17,
-    borderRadius: 5,
+    borderRadius: 32,
     borderWidth: 1,
     borderStyle: 'solid',
     borderColor: '#bcbcbc',
@@ -119,5 +182,11 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#fcfcfc'
 
+  },
+  loadingText: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#003d5b'
   }
 })
--- screens/Result.js
+++ screens/Result.js
@@ -2,21 +2,28 @@ import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Home from './Home'
 
-export default function Result({navigation}) {
-  return (
-    <View>
+export default function Result({navigation, route}) {
 
-      <View>
-        <Text>This will give you result</Text>
+  const {score} = route.params
+
+  const scoreImage = score >= 50 ? require('../static/victory.png') : require('../static/failure.png')
+
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>RESULT</Text>
       </View>
+
+      <Text style={styles.scoreValue}>{score} / 100</Text>
 
       <View style={styles.bannerImage}>
-        <Image source={require('../static/quizbanner.png')} style={styles.imageContainer} resizeMode='contain' />
+        <Image source={scoreImage} style={styles.imageContainer} resizeMode='contain' />
       </View>
 
-      <View>
-        <TouchableOpacity onPress={() => navigation.navigate(Home)}>
-          <Text>Back to home</Text>
+      <View style={styles.buttons} onPress={() => navigation.navigate(Home)}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Back to home</Text>
         </TouchableOpacity>
       </View>
 
@@ -26,12 +33,59 @@ export default function Result({navigation}) {
 
 const styles = StyleSheet.create({
   bannerImage: {
-      marginTop: -20,
       justifyContent: "center",
       alignItems: 'center',
+      flex: 1
   },
   imageContainer: {
       height: 300,
       width: 300,
+  },
+  container: {
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    height: '100%',
+  },
+  buttons: {
+    marginBottom: 12,
+    paddingVertical: 16,
+    justifyContent: "space-between",
+    flexDirection: 'row'
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#edae49',
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20,
+    padding: 10,
+    paddingHorizontal: 17,
+    borderRadius: 32,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#bcbcbc',
+    marginBottom: 20,
+  },
+  buttonText: {
+      fontSize: 17,
+      color: '#fff',
+      fontWeight: '800',
+  },
+  textTitle: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#003d5b'
+  },
+  textContainer: {
+    paddingVertical: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  scoreValue:{
+    fontSize: 24,
+    fontWeight:'800',
+    alignSelf:'center'
   }
 })
